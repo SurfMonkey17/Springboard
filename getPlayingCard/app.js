4e6e5cfb08ec6ea4a46a0368ef02fae54a70bacf
Sherry Fisher <sherryw17@gmail.com>
2025-08-17 23:14:03 -0500

    changed nums to strings in cardValue array

--- getPlayingCard/app.js
+++ getPlayingCard/app.js
@@ -13,7 +13,7 @@
 //Accepts no arguments
 
 function getCard(){
-    const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+    const cardValue = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
     const cardSuit = ["clubs", "spades", "hearts", "diamonds"]; 
 
     let randomCard = {}; 
