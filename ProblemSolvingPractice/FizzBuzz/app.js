1bd57ab99c751de0f15e7c002cefddb90e8a220e
Sherry Fisher <sherryw17@gmail.com>
2025-08-18 07:20:42 -0500

    modified to include every number up to num

--- ProblemSolvingPractice/FizzBuzz/app.js
+++ ProblemSolvingPractice/FizzBuzz/app.js
@@ -1,45 +1,46 @@
 //FizzBuzz
 
-//Given positive int < 100. 
+//If given num is not positive int < 100, print "Error":
 
 function fizzBuzz(num) {
-    if (num > 0 && num < 100) {
-
-//For every number up to n (included):
-
-//if number divisible by 3 and 5, print "FizzBuzz":
-        if (num % 3 === 0 && num % 5 === 0) {
-            console.log ("FizzBuzz");
-        }
-
-//Else if the number is divisible by 3, print "Fizz":
-        else if (num % 3 === 0) {
-            console.log("Fizz");
-        }
-
-//Else if the number is divisible by 5, print "Buzz": 
-        else if (num % 5 === 0) {
-            console.log("Buzz")
-        }
-    
-
-//Else if not divisible by 3 or 5, print the number: 
-        else { 
-            console.log(num);
-        }
-    }
-
-
-//Else: print "Error" and stop the execution:
-    else {
+    if (num < 1 || num > 99) {
         console.log("Error");
     }
 
+//For every number up to n (included):
+
+    else {
+    
+        for (let i = 1; i <= num; i++){
+
+            //if i divisible by 3 and 5, print "FizzBuzz":
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log ("FizzBuzz");
+            }
+
+            //Else if i is divisible by 3, print "Fizz":
+            else if (i % 3 === 0) {
+                console.log("Fizz");
+            }
+
+            //Else if i is divisible by 5, print "Buzz": 
+            else if (i % 5 === 0) {
+                console.log("Buzz")
+            }
+    
+
+            //Else if not divisible by 3 or 5, print the number: 
+            else { 
+                console.log(i);
+            }
+        }
+    }
 }
 
-fizzBuzz(15);
-fizzBuzz(33);
-fizzBuzz(50);
-fizzBuzz(79);
-fizzBuzz(100);
+fizzBuzz(16);
+// fizzBuzz(50);
+// fizzBuzz(79);
+// fizzBuzz(100);
+// fizzBuzz(0);
+// fizzBuzz(-1);
 
