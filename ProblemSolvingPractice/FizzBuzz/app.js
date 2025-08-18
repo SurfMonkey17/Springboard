88038cf92cd480faf8f24aee12f21d7194f851ca
Sherry Fisher <sherryw17@gmail.com>
2025-08-18 00:01:16 -0500

    added Problem Solving Practice Folder and setup for FizzBuzz project

---
+++ ProblemSolvingPractice/FizzBuzz/app.js
@@ -0,0 +1,10 @@
+//FizzBuzz
+//Given positive int < 100. 
+//For every number up to n (included):
+//Print Fizz if the number is divisible by 3. 
+//Print Buzz if the number is divisible by 5. 
+//Print FizzBuzz if the number is divisible by both 3 and 5. 
+//Print the number if it is not divisible by 3 or 5. 
+//Print Error if unexpected happens and stop the execution. 
+//Write a function fizzBuzz using JavaScript that solves the above problem and satisfies the conditions. 
+
