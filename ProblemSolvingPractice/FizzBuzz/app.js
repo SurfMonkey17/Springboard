7c75bdd46eb0f51e28017ec7945825fa0d794434
Sherry Fisher <sherryw17@gmail.com>
2025-08-18 07:04:18 -0500

    completed fizzBuzz project

--- ProblemSolvingPractice/FizzBuzz/app.js
+++ ProblemSolvingPractice/FizzBuzz/app.js
@@ -1,10 +1,45 @@
 //FizzBuzz
+
 //Given positive int < 100. 
+
+function fizzBuzz(num) {
+    if (num > 0 && num < 100) {
+
 //For every number up to n (included):
-//Print Fizz if the number is divisible by 3. 
-//Print Buzz if the number is divisible by 5. 
-//Print FizzBuzz if the number is divisible by both 3 and 5. 
-//Print the number if it is not divisible by 3 or 5. 
-//Print Error if unexpected happens and stop the execution. 
-//Write a function fizzBuzz using JavaScript that solves the above problem and satisfies the conditions. 
+
+//if number divisible by 3 and 5, print "FizzBuzz":
+        if (num % 3 === 0 && num % 5 === 0) {
+            console.log ("FizzBuzz");
+        }
+
+//Else if the number is divisible by 3, print "Fizz":
+        else if (num % 3 === 0) {
+            console.log("Fizz");
+        }
+
+//Else if the number is divisible by 5, print "Buzz": 
+        else if (num % 5 === 0) {
+            console.log("Buzz")
+        }
+    
+
+//Else if not divisible by 3 or 5, print the number: 
+        else { 
+            console.log(num);
+        }
+    }
+
+
+//Else: print "Error" and stop the execution:
+    else {
+        console.log("Error");
+    }
+
+}
+
+fizzBuzz(15);
+fizzBuzz(33);
+fizzBuzz(50);
+fizzBuzz(79);
+fizzBuzz(100);
 
