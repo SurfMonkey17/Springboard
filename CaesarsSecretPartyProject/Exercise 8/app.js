8d8a4555fd717e6cddd51824d4b10436e2be026b
Sherry Fisher <sherryw17@gmail.com>
2025-08-19 14:32:05 -0500

    added encryptMessage and decryptLetter functions

--- CaesarsSecretPartyProject/Exercise 8/app.js
+++ CaesarsSecretPartyProject/Exercise 8/app.js
@@ -6,10 +6,40 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 function encryptLetter (letter, shiftValue) {
 
-    let encryptedLetter = (alphabet.indexOf(letter.toLowerCase()) + shiftValue) % alphabet.length;
+    let encryptedLetters = alphabet[(alphabet.indexOf(letter.toLowerCase()) + shiftValue) % alphabet.length];
 
-    return encryptedLetter;
+    return encryptedLetters;
 
 }
 
+//Step 2: Create a function named encryptMessage that takes a word and a shift value as parameters and returns the encrypted version of the entire word: 
+
+function encryptMessage (word, shiftValue) {
+
+    let encryptedWord = "";
+
+    for (let i = 0; i < word.length; i++) {
+        
+        encryptedWord += encryptLetter(word[i], shiftValue).toUpperCase(); 
+    }
+    
+    return encryptedWord;
+    
+}
+
+//Step 3: Create a function named decryptLetter that takes an encrypted letter and a shift value as parameters and return the decrypted version of the letter: 
+
+function decryptLetter(encryptedLetter, shiftValue) {
+
+    let decryptedLetter = alphabet[((alphabet.indexOf(encryptedLetter.toLowerCase()) - shiftValue) + alphabet.length) % alphabet.length];
+
+    return decryptedLetter;
+}
+
+
+
+
+
 console.log(encryptLetter("d", 10));
+console.log(encryptMessage("BRUTUS", 3));
+console.log(decryptLetter("b", 3));
