83418787be981cbf30370c69784c37b040b89110
Sherry Fisher <sherryw17@gmail.com>
2025-09-02 18:20:02 -0500

    completed mousecolormap

--- MouseColorMap/app.js
+++ MouseColorMap/app.js
@@ -1,10 +1,28 @@
 
+//HINTS:
+//document.addEventListener("")
+
+//window.innerWidth
+//window.innerHeight
+
+//rgb(0, 0, 0) use coordinates, one for red and other for blue. 
 
 document.addEventListener("mousemove", function(event){
+    const mouseX = Math.round((event.pageX * 255)/window.innerWidth); 
+    const mouseY = Math.round((event.pageY * 255)/window.innerHeight);
+    const color = `rgb(${mouseX}, 0, ${mouseY}`;
+    document.body.style.backgroundColor = color;
+})
 
-    const mouseX = event.pageX;
-    const mouseY = event.pageY; 
 
-    document.body.style.backgroundColor = `#${mouseX}${mouseY}`;
-    });
-    
+
+//My initial solution before hints: 
+
+// document.addEventListener("mousemove", function(event){
+
+//     const mouseX = event.pageX;
+//     const mouseY = event.pageY; 
+
+
+//     document.body.style.backgroundColor = `#${mouseX}${mouseY}`;
+//     });
