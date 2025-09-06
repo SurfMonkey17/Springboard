b4b648a5f5fee58b5f06aa05378a3ce51b76c702
Sherry Fisher <sherryw17@gmail.com>
2025-09-06 14:51:08 -0500

    updated h1 to display new random color rgb

--- RandomColorGenerator/app.js
+++ RandomColorGenerator/app.js
@@ -1,9 +1,12 @@
 const btn = document.querySelector('#change-color-button');
+const h1 = document.querySelector('h1')
 
 btn.addEventListener("click", function(event){
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256); 
-    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256); 
+    const newColor = `rgb(${r}, ${g}, ${b})`;
+    document.body.style.backgroundColor = newColor
+    h1.innerText = newColor;
 })
 
