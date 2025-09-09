3d12017b4f9d764f3fd598b0ada99f02b0b85382
Sherry Fisher <sherryw17@gmail.com>
2025-09-08 20:10:47 -0500

    updated JS to add boxes and color

--- events/script.js
+++ events/script.js
@@ -1,21 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
-//add elements you will need select for
-const boxContainer = document.querySelector("#box-container");
-const newBoxButton = document.querySelector("#new-box-button");
-const colorForm = document.querySelector("#color-form");
-const colorInput = document.querySelector("#color-input");
-const boxColor;
-const counter;
+    const colorForm = document.querySelector("#color-form");
+    const colorInput = document.querySelector("#color-input");
+    const boxContainer = document.querySelector("#box-container");
+    const newBoxButton = document.querySelector("#new-box-button");
+    const box = document.querySelector("#box");
 
-//
-
-document.boxContainer.addEventListener("click", function(event){
-    document.box.backgroundColor = ""
-})
-
-
-
-
-
-    
-})
+    colorForm.addEventListener("submit", function(e){
+        e.preventDefault();
+        const boxColor = colorInput.value;
+        const newBox = document.createElement("button");
+        newBox.setAttribute("class", "box");
+        newBox.style.backgroundColor = boxColor;
+        boxContainer.append(newBox);     
+    });
+});
