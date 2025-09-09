45ae2e780f0805057ce9431995cef216939618d6
Sherry Fisher <sherryw17@gmail.com>
2025-09-08 21:17:03 -0500

    added new box function and updated new color function

--- events/script.js
+++ events/script.js
@@ -3,14 +3,42 @@ document.addEventListener("DOMContentLoaded", function(){
     const colorInput = document.querySelector("#color-input");
     const boxContainer = document.querySelector("#box-container");
     const newBoxButton = document.querySelector("#new-box-button");
-    const box = document.querySelector("#box");
+    let boxColor = null;
+    let boxID = 0;
+
+    function newBox(){
+        const newBox = document.createElement("div");
+        newBox.setAttribute("class", "box");
+        newBox.innerText = `Box ${boxID}`
+        boxID++; 
+        newBox.style.backgroundColor = boxColor; 
+        boxContainer.append(newBox);
+    }
 
     colorForm.addEventListener("submit", function(e){
         e.preventDefault();
-        const boxColor = colorInput.value;
-        const newBox = document.createElement("button");
-        newBox.setAttribute("class", "box");
-        newBox.style.backgroundColor = boxColor;
-        boxContainer.append(newBox);     
-    });
+        const newColor = colorInput.value.trim();
+        const boxes = document.querySelectorAll(".box");
+        for (let box of boxes){
+            box.style.backgroundColor = newColor;
+        }
+        boxColor = newColor; 
+        colorInput.value = "";
+    })
+
+    newBoxButton.addEventListener("click", function(e){
+        newBox();
+    })
+  
 });
+
+
+
+//   colorForm.addEventListener("submit", function(e){
+//         e.preventDefault();
+//         const boxColor = colorInput.value;
+//         const newBox = document.createElement("button");
+//         newBox.setAttribute("class", "box");
+//         newBox.style.backgroundColor = boxColor;
+//        
+//     });
