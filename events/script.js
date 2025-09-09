eb37903cf2d6a8fc2263eb456045c9c319de2bd9
Sherry Fisher <sherryw17@gmail.com>
2025-09-08 22:19:26 -0500

    added comments

--- events/script.js
+++ events/script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function(){
     let boxColor = null;
     let boxID = 0;
 
+    //function to create new box 
     function newBox(){
         const newBox = document.createElement("div");
         newBox.setAttribute("class", "box");
@@ -16,9 +17,10 @@ document.addEventListener("DOMContentLoaded", function(){
         boxID++; 
     };
 
+    //allow user to set color on new and old boxes: 
     colorForm.addEventListener("submit", function(e){
         e.preventDefault();
-        const newColor = colorInput.value.trim();
+        const newColor = colorInput.value.trim();  //removes whitespaces
         const boxes = document.querySelectorAll(".box");
         for (let box of boxes){
             box.style.backgroundColor = newColor;
@@ -27,22 +29,26 @@ document.addEventListener("DOMContentLoaded", function(){
         colorInput.value = "";
     });
 
+    //calls newBox function when new box button is clicked and creates new box: 
     newBoxButton.addEventListener("click", function(e){
         newBox();
     });
 
+    //remove box when double clicked: 
     document.addEventListener("dblclick", function(e){
         if (e.target.classList.contains("box")){
             e.target.remove();
         };
     });
 
+    //shows coordinates when mouse moves over box: 
     document.addEventListener("mouseover", function(e){
         if (e.target.classList.contains("box")){
             e.target.innerText = `x: ${e.pageX}, y: ${e.pageY}`;
         };
     });
 
+    //displays box ID again when mouse moves away from box: 
     document.addEventListener("mouseout", function(e){
         if (e.target.classList.contains("box")){
             const boxID = e.target.getAttribute("data-id")
@@ -50,8 +56,9 @@ document.addEventListener("DOMContentLoaded", function(){
         };
     });
 
+    //allow user to create new boxes with the n or N key press:
     document.addEventListener("keydown", function(e){
-        if (e.target.id === "color-input"){
+        if (e.target.id === "color-input"){           //ignores the n key press in the input field. 
             return; 
         };
 
